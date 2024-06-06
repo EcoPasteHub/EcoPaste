9b3b59b8ea81e3cb467c1b7027881d7a2a120c45
ayang <473033518@qq.com>
2024-06-06 21:40:48 +0800

    feat: 支持剪切板变化存放至数据库

--- src/pages/Clipboard/History/index.tsx
+++ src/pages/Clipboard/History/index.tsx
@@ -1,8 +1,56 @@
 import { appWindow } from "@tauri-apps/api/window";
+import {
+	onSomethingUpdate,
+	readFilesURIs,
+	readHtml,
+	readImageBase64,
+	readRtf,
+	readText,
+	startListening,
+} from "tauri-plugin-clipboard-api";
 
 const ClipboardWindow = () => {
 	useMount(() => {
 		frostedWindow();
+
+		startListening();
+
+		onSomethingUpdate(async (updateTypes) => {
+			if (updateTypes.files) {
+				return insertSQL("history", {
+					type: "files",
+					content: JSON.stringify(await readFilesURIs()),
+				});
+			}
+
+			if (updateTypes.image) {
+				return insertSQL("history", {
+					type: "image",
+					content: `data:image/png;base64, ${await readImageBase64()}`,
+				});
+			}
+
+			if (updateTypes.html) {
+				return insertSQL("history", {
+					type: "html",
+					content: await readHtml(),
+				});
+			}
+
+			if (updateTypes.rtf) {
+				return insertSQL("history", {
+					type: "rtf",
+					content: await readRtf(),
+				});
+			}
+
+			if (updateTypes.text) {
+				insertSQL("history", {
+					type: "text",
+					content: await readText(),
+				});
+			}
+		});
 	});
 
 	return (
