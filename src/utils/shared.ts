89f614ea4a8dd25e73d41a0230bae0e94d34cbf5
青菜白玉汤 <79054161+Azir-11@users.noreply.github.com>
2024-06-20 23:14:39 +0800

    perf: windows系统切换主题允许取消 (#7)

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -1,15 +1,18 @@
 import type { Theme } from "@/types/store";
 import { invoke } from "@tauri-apps/api";
-import { message } from "@tauri-apps/api/dialog";
+import { ask } from "@tauri-apps/api/dialog";
 
 /**
  * 切换主题
  */
 export const toggleTheme = async (theme: Theme) => {
 	if (await isWin()) {
-		await message("切换主题需要重启 app 才能生效！", {
+		const yes = await ask("切换主题需要重启 app 才能生效！", {
 			okLabel: "重启",
+			cancelLabel: "取消",
 		});
+
+		if (!yes) return;
 	}
 
 	globalStore.theme = theme;
