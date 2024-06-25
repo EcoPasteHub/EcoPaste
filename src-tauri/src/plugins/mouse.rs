eea89ecc0aab2283239a3caeb813350efac47bf2
ayang <473033518@qq.com>
2024-06-25 08:21:42 +0800

    style: 代码格式化

--- src-tauri/src/plugins/mouse.rs
+++ src-tauri/src/plugins/mouse.rs
@@ -3,16 +3,17 @@ use mouse_position::mouse_position::Mouse;
 use tauri::{
     command, generate_handler,
     plugin::{Builder, TauriPlugin},
-    Result, Wry,Error
+    Error, Result, Wry,
 };
 
 #[command]
 async fn get_mouse_coords() -> Result<(i32, i32)> {
-  let position = Mouse::get_mouse_position();
+    let position = Mouse::get_mouse_position();
+
     match position {
-        Mouse::Position { x, y } => {Ok((x,y))},
+        Mouse::Position { x, y } => Ok((x, y)),
         Mouse::Error => Err(Error::InvokeKey),
-   }
+    }
 }
 
 pub fn init() -> TauriPlugin<Wry> {
