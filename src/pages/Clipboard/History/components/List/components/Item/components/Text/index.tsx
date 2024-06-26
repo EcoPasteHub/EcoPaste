554c1d6fd633b9a87f831ff1758f8a65d06a310b
Azir <2075125282@qq.com>
2024-06-26 23:36:59 +0800

    feat: 去除 url/email 默认触发操作

--- src/pages/Clipboard/History/components/List/components/Item/components/Text/index.tsx
+++ src/pages/Clipboard/History/components/List/components/Item/components/Text/index.tsx
@@ -33,16 +33,6 @@ const Text: FC<HistoryItem> = (props) => {
 			return renderColor();
 		}
 
-		if (isURL(value) || isEmail(value)) {
-			const url = isURL(value) ? value : `mailto:${value}`;
-
-			return (
-				<a href={url} target="_blank" rel="noreferrer">
-					{value}
-				</a>
-			);
-		}
-
 		return value;
 	};
 
