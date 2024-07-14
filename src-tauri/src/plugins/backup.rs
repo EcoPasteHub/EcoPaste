a8859ef34d1b7165a09f1d96e8b16fce4a5e6148
ayang <473033518@qq.com>
2024-07-15 00:14:47 +0800

    refactor: 重写了导入和导出的方法，让它适用于全平台

--- src-tauri/src/plugins/backup.rs
+++ src-tauri/src/plugins/backup.rs
@@ -1,48 +1,37 @@
 use super::fs_extra::preview_path;
-use std::{
-    fs::File,
-    io::{self, BufReader},
-};
+use flate2::{read::GzDecoder, write::GzEncoder, Compression};
+use std::fs::{read_dir, File};
+use tar::Archive;
 use tauri::{
     api::path::{app_data_dir, download_dir},
     command, generate_handler,
     plugin::{Builder, TauriPlugin},
     AppHandle, Result, Wry,
 };
-use walkdir::WalkDir;
-use zip::{read::ZipArchive, write::SimpleFileOptions, CompressionMethod, ZipWriter};
 
 #[command]
 async fn export_data(app_handle: AppHandle, file_name: String) -> Result<()> {
-    let entry_dir = app_data_dir(&app_handle.config()).unwrap();
-    let output_path = download_dir().unwrap().join(file_name);
+    let src_dir = app_data_dir(&app_handle.config()).unwrap();
+    let dst_path = download_dir().unwrap().join(file_name.clone());
+    let dst_file = File::create(dst_path.clone())?;
 
-    let file = File::create(output_path.clone()).unwrap();
-    let mut zip = ZipWriter::new(file);
-    let walkdir = WalkDir::new(entry_dir.clone());
-    let it = walkdir.into_iter();
+    let enc = GzEncoder::new(dst_file, Compression::default());
+    let mut tar = tar::Builder::new(enc);
 
-    let options = SimpleFileOptions::default()
-        .compression_method(CompressionMethod::Deflated)
-        .unix_permissions(0o755);
-
-    for entry in it {
-        let entry = entry.unwrap();
+    for entry in read_dir(&src_dir)? {
+        let entry = entry?;
         let path = entry.path();
-        let name = path.strip_prefix(entry_dir.clone()).unwrap();
-
-        if path.is_file() {
-            zip.start_file(name.to_string_lossy(), options).unwrap();
-            let mut f = File::open(path)?;
-            std::io::copy(&mut f, &mut zip)?;
-        } else if !name.as_os_str().is_empty() {
-            zip.add_directory(name.to_string_lossy(), options).unwrap();
+        let name = path.strip_prefix(&src_dir).unwrap();
+        if path.is_dir() {
+            tar.append_dir_all(name, path.clone())?;
+        } else {
+            tar.append_file(name, &mut File::open(path.clone())?)?;
         }
     }
 
-    zip.finish().unwrap();
+    tar.finish()?;
 
-    preview_path(output_path.to_str().unwrap(), true)
+    preview_path(dst_path.to_str().unwrap(), true)
         .await
         .unwrap();
 
@@ -51,39 +40,20 @@ async fn export_data(app_handle: AppHandle, file_name: String) -> Result<()> {
 
 #[command]
 async fn import_data(app_handle: AppHandle, path: String) -> Result<bool> {
-    let out_dir = app_data_dir(&app_handle.config()).unwrap();
+    let dst_dir = app_data_dir(&app_handle.config()).unwrap();
 
-    let zip_file = File::open(path).unwrap();
-    let mut archive = ZipArchive::new(BufReader::new(zip_file)).unwrap();
+    let file = File::open(path)?;
+    let decoder = GzDecoder::new(file);
+    let mut archive = Archive::new(decoder);
 
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
-        let outpath = match file.enclosed_name() {
-            Some(path) => out_dir.join(path),
-            None => continue,
-        };
+    for entry in archive.entries()? {
+        let mut entry = entry?;
+        let path = entry.path()?;
 
-        if file.is_dir() {
-            std::fs::create_dir_all(&outpath).unwrap();
-        } else {
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    std::fs::create_dir_all(&p).unwrap();
-                }
-            }
-            let mut outfile = File::create(&outpath).unwrap();
-            io::copy(&mut file, &mut outfile)?;
-        }
+        #[cfg(target_os = "windows")]
+        let path = path::Path::new(&path.to_string_lossy().replace("\\", "/")).to_path_buf();
 
-        // 设置解压后的文件的权限
-        #[cfg(unix)]
-        {
-            use std::os::unix::fs::PermissionsExt;
-
-            if let Some(mode) = file.unix_mode() {
-                std::fs::set_permissions(&outpath, std::fs::Permissions::from_mode(mode))?;
-            }
-        }
+        entry.unpack(dst_dir.join(path))?;
     }
 
     Ok(true)
