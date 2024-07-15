8f519072cd138de0c3cdd59d2586ebd05b44cfea
ayang <75017711+ayangweb@users.noreply.github.com>
2024-07-15 22:25:16 +0800

    fix: 修复 HTML 内容中固定定位元素导致的布局问题 (#125)

--- src/pages/Clipboard/History/components/List/components/Item/components/HTML/index.tsx
+++ src/pages/Clipboard/History/components/List/components/Item/components/HTML/index.tsx
@@ -4,7 +4,12 @@ import type { FC } from "react";
 const HTML: FC<HistoryItem> = (props) => {
 	const { value = "" } = props;
 
-	return <div dangerouslySetInnerHTML={{ __html: value }} />;
+	return (
+		<div
+			dangerouslySetInnerHTML={{ __html: value }}
+			className="translate-z-0"
+		/>
+	);
 };
 
 export default memo(HTML);
