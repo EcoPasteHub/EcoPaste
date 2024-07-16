22de6667a757c47d6b78558799bd8e877fb209b1
zhenyuWang <13641039885@163.com>
2024-07-16 11:17:06 +0800

    style: 优化滚动条宽度 (#98)

--- src/components/Scrollbar/index.tsx
+++ src/components/Scrollbar/index.tsx
@@ -9,8 +9,32 @@ const Scrollbar = forwardRef<HTMLElement, MacScrollbarProps>((props, ref) => {
 
 	useImperativeHandle(ref, () => containerRef.current!);
 
+	const scrollBarWidth = "4px";
+
 	return (
-		<MacScrollbar ref={containerRef} skin={isDark ? "dark" : "light"} {...rest}>
+		<MacScrollbar
+			ref={containerRef}
+			skin={isDark ? "dark" : "light"}
+			{...rest}
+			thumbStyle={(horizontal) =>
+				horizontal
+					? {
+							height: scrollBarWidth,
+						}
+					: {
+							width: scrollBarWidth,
+						}
+			}
+			trackStyle={(horizontal) =>
+				horizontal
+					? {
+							height: scrollBarWidth,
+						}
+					: {
+							width: scrollBarWidth,
+						}
+			}
+		>
 			{children}
 		</MacScrollbar>
 	);
