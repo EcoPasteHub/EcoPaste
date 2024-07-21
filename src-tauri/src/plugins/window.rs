5e0913ba8b9468527432f0e05f7339bae99e0949
ayang <75017711+ayangweb@users.noreply.github.com>
2024-07-21 18:43:40 +0800

    refactor: 更新了一些国际化翻译 (#171)

--- src-tauri/src/plugins/window.rs
+++ src-tauri/src/plugins/window.rs
@@ -1,5 +1,3 @@
-use active_win_pos_rs::get_active_window;
-use std::sync::Mutex;
 use tauri::{
     command, generate_handler,
     plugin::{Builder, TauriPlugin},
@@ -11,9 +9,6 @@ use tauri::{
 pub static MAIN_WINDOW_LABEL: &str = "main";
 // 偏好设置窗口的label
 pub static PREFERENCE_WINDOW_LABEL: &str = "preference";
-
-// 上一个窗口的进程 id
-static PREVIOUS_PROCESS_ID: Mutex<u64> = Mutex::new(0);
 
 // 创建窗口
 #[command]
@@ -32,14 +27,11 @@ pub async fn create_window(app_handle: AppHandle, label: String, mut options: Wi
 // 显示窗口
 #[command]
 pub async fn show_window(window: Window) {
-    let label = window.label();
+    #[cfg(target_os = "macos")]
+    {
+        use super::paste::get_previous_process_id;
 
-    if label == MAIN_WINDOW_LABEL {
-        let active_window = get_active_window().unwrap();
-
-        let mut previous_process_id = PREVIOUS_PROCESS_ID.lock().unwrap();
-
-        *previous_process_id = active_window.process_id;
+        get_previous_process_id(window.clone());
     }
 
     #[cfg(not(target_os = "linux"))]
@@ -71,53 +63,6 @@ pub async fn hide_window(window: Window) {
 pub async fn set_window_shadow(_window: Window) {
     #[cfg(not(target_os = "linux"))]
     window_shadows::set_shadow(&_window, true).unwrap();
-}
-
-// 让上一个窗口聚焦
-pub fn focus_previous_window() {
-    let process_id = *PREVIOUS_PROCESS_ID.lock().unwrap();
-
-    if process_id == 0 {
-        return;
-    }
-
-    #[cfg(target_os = "macos")]
-    {
-        use cocoa::{
-            appkit::{
-                NSApplicationActivationOptions::NSApplicationActivateIgnoringOtherApps,
-                NSRunningApplication,
-            },
-            base::nil,
-        };
-
-        unsafe {
-            let app = NSRunningApplication::runningApplicationWithProcessIdentifier(
-                nil,
-                process_id as i32,
-            );
-
-            app.activateWithOptions_(NSApplicationActivateIgnoringOtherApps);
-        }
-    }
-
-    #[cfg(target_os = "windows")]
-    {
-        use winapi::um::winuser::{GetForegroundWindow, SetForegroundWindow};
-
-        unsafe {
-            let hwnd = GetForegroundWindow();
-
-            if hwnd.is_null() {
-                println!("Could not get active window");
-                return;
-            }
-
-            if SetForegroundWindow(hwnd) == 0 {
-                println!("Could not focus on window");
-            }
-        }
-    }
 }
 
 pub fn init() -> TauriPlugin<Wry> {
--- src/pages/Settings/components/ThemeMode/index.tsx
+++ src/pages/Settings/components/ThemeMode/index.tsx
@@ -12,22 +12,22 @@ const ThemeMode = () => {
 
 	const options: Option[] = [
 		{
-			label: t("preference.settings.basic.label.theme_mode_auto"),
+			label: t("preference.settings.basic.label.theme_auto"),
 			value: "auto",
 		},
 		{
-			label: t("preference.settings.basic.label.theme_mode_light"),
+			label: t("preference.settings.basic.label.theme_light"),
 			value: "light",
 		},
 		{
-			label: t("preference.settings.basic.label.theme_mode_dark"),
+			label: t("preference.settings.basic.label.theme_dark"),
 			value: "dark",
 		},
 	];
 
 	return (
 		<Flex align="center">
-			<span>{t("preference.settings.basic.label.theme_mode")}：</span>
+			<span>{t("preference.settings.basic.label.theme")}：</span>
 			<Segmented value={theme} options={options} onChange={toggleTheme} />
 		</Flex>
 	);
