1dedbd6f68bb31b2b1d4b21434f630b71351150b
ayang <473033518@qq.com>
2024-07-24 00:37:29 +0800

    refactor: 固定窗口自动粘贴时不隐藏窗口

--- src/hooks/useFocus.ts
+++ src/hooks/useFocus.ts
@@ -1,5 +1,4 @@
 import { appWindow } from "@tauri-apps/api/window";
-import type { Timeout } from "ahooks/lib/useRequest/src/types";
 
 interface Props {
 	onFocus?: () => void;
@@ -7,38 +6,33 @@ interface Props {
 }
 
 interface State {
-	delay: number;
-	timer?: Timeout;
 	unlisten?: () => void;
 }
 
 export const useFocus = (props: Props) => {
 	const { onFocus, onBlur } = props;
 
-	const state = useReactive<State>({
-		delay: 0,
-	});
+	const state = useReactive<State>({});
+
+	const { run } = useDebounceFn(
+		({ payload }) => {
+			if (payload) {
+				onFocus?.();
+			} else {
+				onBlur?.();
+			}
+		},
+		{
+			wait: isMac() ? 0 : 100,
+		},
+	);
 
 	useMount(async () => {
-		if (isWin()) {
-			state.delay = 100;
-		}
-
-		state.unlisten = await appWindow.onFocusChanged(({ payload }) => {
-			clearTimeout(state.timer);
-
-			/**
-			 * 背景：在 Windows 系统上，拖动窗口会多次触发 `onFocusChanged` 事件，导致窗口被失焦关闭
-			 * 解决方案：给 `onFocusChanged` 事件加个定时器，用最新的状态做变更
-			 */
-			state.timer = setTimeout(() => {
-				if (payload) {
-					onFocus?.();
-				} else {
-					onBlur?.();
-				}
-			}, state.delay);
-		});
+		/**
+		 * 背景：在 Windows 系统上，拖动窗口会多次触发 `onFocusChanged` 事件，导致窗口被失焦关闭
+		 * 解决方案：给 `onFocusChanged` 事件加个防抖，用最新的状态做变更
+		 */
+		state.unlisten = await appWindow.onFocusChanged(run);
 	});
 
 	useUnmount(() => {
