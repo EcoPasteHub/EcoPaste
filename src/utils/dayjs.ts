c778a8539dcdbbe83c5a80d25c607ea89282c6a2
amaoo <lzmyrs@gmail.com>
2024-07-25 18:14:29 +0800

    feat: dayjs.ts 引入 ja

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
 import "dayjs/locale/zh-tw";
+import "dayjs/locale/ja";
 import relativeTime from "dayjs/plugin/relativeTime";
 import utc from "dayjs/plugin/utc";
 
