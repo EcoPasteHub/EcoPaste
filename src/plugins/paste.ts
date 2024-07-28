884d798774c8917a494bbb1a4c93e74893ad1e08
ayang <473033518@qq.com>
2024-07-28 22:19:51 +0800

    refactor: windows 的固定窗口自动粘贴暂时以隐藏再显示解决

--- src/plugins/paste.ts
+++ src/plugins/paste.ts
@@ -3,6 +3,6 @@ import { invoke } from "@tauri-apps/api";
 /**
  * 粘贴剪切板内容
  */
-export const paste = () => {
-	invoke(PASTE_PLUGIN.PASTE);
+export const paste = async () => {
+	return await invoke(PASTE_PLUGIN.PASTE);
 };
