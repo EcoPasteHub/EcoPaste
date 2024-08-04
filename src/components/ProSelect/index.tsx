063924e2b00bd2cbf37c290377b81f9986ecd81b
ayang <473033518@qq.com>
2024-08-04 21:15:29 +0800

    feat: 优化下拉框显示，使其宽度根据最长选项自动调整

--- src/components/ProSelect/index.tsx
+++ src/components/ProSelect/index.tsx
@@ -7,9 +7,32 @@ export type ProSelectProps<T> = SelectProps<T> & ListItemMetaProps;
 const ProSelect = <T,>(props: ProSelectProps<T>) => {
 	const { title, description, children, ...rest } = props;
 
+	const width = useCreation(() => {
+		let width = 0;
+
+		const canvas = document.createElement("canvas");
+		const context = canvas.getContext("2d");
+
+		if (!context) return 100;
+
+		context.font = getComputedStyle(document.body).font;
+
+		for (const option of rest.options!) {
+			const textWidth = context.measureText(option.label as string).width;
+
+			if (textWidth > width) {
+				width = textWidth;
+			}
+		}
+
+		canvas.remove();
+
+		return width + 43;
+	}, [rest.options]);
+
 	return (
 		<ProListItem title={title} description={description}>
-			<Select {...rest} />
+			<Select {...rest} style={{ width }} />
 
 			{children}
 		</ProListItem>
