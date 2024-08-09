81823a851450a77e4d4ab7cf4e474199ecb219c8
zpp0196 <zpp0196@gmail.com>
2024-08-09 12:36:39 +0800

    feat: linux 支持图片 ocr 功能，需自行安装 `tesseract` (#334)

--- src-tauri/src/plugins/ocr.rs
+++ src-tauri/src/plugins/ocr.rs
@@ -5,6 +5,7 @@ use tauri::{
 };
 
 #[command]
+#[cfg(not(target_os = "linux"))]
 async fn system_ocr(path: &str) -> Result<String> {
     use tauri::api::process::{Command, CommandEvent};
 
@@ -23,6 +24,36 @@ async fn system_ocr(path: &str) -> Result<String> {
             _ => return Ok("".to_string()),
         }
     }
+}
+
+#[command]
+#[cfg(target_os = "linux")]
+async fn system_ocr(path: &str) -> Result<String> {
+    let output = match std::process::Command::new("tesseract")
+        .arg(path)
+        .arg("stdout")
+        .args(["-l", "eng+chi_sim+jpn"])
+        .output()
+    {
+        Ok(output) => output,
+        Err(e) => {
+            log::error!("Failed to exec tesseract: {:?}", e);
+            return Ok(String::default());
+        }
+    };
+
+    let text = if output.status.success() {
+        String::from_utf8(output.stdout)
+            .unwrap_or_default()
+            .split_whitespace()
+            .collect()
+    } else {
+        let content = String::from_utf8(output.stderr).unwrap_or_default();
+        log::error!("Error while executing tesseract: {:?}", content);
+        String::default()
+    };
+
+    Ok(text)
 }
 
 pub fn init() -> TauriPlugin<Wry> {
