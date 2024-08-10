d563d701e87121411a0a3facbab2fc6e07408866
zpp0196 <zpp0196@gmail.com>
2024-08-10 20:00:49 +0800

    refactor: 优化 linux 前台窗口监听

--- src-tauri/src/core/app/linux.rs
+++ src-tauri/src/core/app/linux.rs
@@ -1,43 +1,82 @@
 use std::sync::Mutex;
 
-use x11::xlib::{self, XDefaultRootWindow, XGetInputFocus, XNextEvent, XOpenDisplay, XSelectInput};
+use x11::xlib::{
+    self, Atom, Display, XDefaultRootWindow, XFree, XGetInputFocus, XGetWindowProperty,
+    XInternAtom, XNextEvent, XOpenDisplay, XSelectInput,
+};
 
-static FOREMOST_APPS: Mutex<Vec<u64>> = Mutex::new(Vec::new());
+fn get_net_wm_name(display: *mut Display, window: u64) -> std::result::Result<String, String> {
+    let mut actual_type: Atom = 0;
+    let mut actual_format: i32 = 0;
+    let mut nitems: u64 = 0;
+    let mut bytes_after: u64 = 0;
+    let mut prop: *mut u8 = std::ptr::null_mut();
+    let net_wm_name_atom =
+        unsafe { XInternAtom(display, b"_NET_WM_NAME\0".as_ptr() as _, xlib::False) };
+    let result = unsafe {
+        XGetWindowProperty(
+            display,
+            window,
+            net_wm_name_atom,
+            0,
+            1024,
+            xlib::False,
+            xlib::AnyPropertyType as _,
+            &mut actual_type,
+            &mut actual_format,
+            &mut nitems,
+            &mut bytes_after,
+            &mut prop,
+        )
+    };
+    if result == xlib::Success as i32 && !prop.is_null() {
+        let name = unsafe {
+            std::ffi::CStr::from_ptr(prop as *const i8)
+                .to_string_lossy()
+                .into_owned()
+        };
+        unsafe { XFree(prop as *mut _) };
+        Ok(name)
+    } else {
+        Err(format!("{}", window))
+    }
+}
+
+static FOREMOST_APPS: Mutex<Option<u64>> = Mutex::new(None);
 
 pub fn observe_app() {
     std::thread::spawn(|| unsafe {
         let display = XOpenDisplay(std::ptr::null_mut());
         if display.is_null() {
-            eprintln!("Could not open display");
+            log::error!("Could not open display");
             return;
         }
 
         let root_window = XDefaultRootWindow(display);
-        XSelectInput(display, root_window, xlib::FocusChangeMask);
+        XSelectInput(
+            display,
+            root_window,
+            xlib::FocusChangeMask | xlib::PropertyChangeMask,
+        );
 
         loop {
             let mut event = std::mem::zeroed();
             XNextEvent(display, &mut event);
 
-            if event.get_type() != xlib::FocusIn {
-                continue;
-            }
-
-            let mut app = FOREMOST_APPS.lock().unwrap();
-
-            if app.len() >= 2 {
-                app.remove(0);
-            }
-
             let mut focus_return: u64 = 0;
             let mut revert_to_return: i32 = 0;
             XGetInputFocus(display, &mut focus_return, &mut revert_to_return);
 
-            app.push(focus_return);
+            if get_net_wm_name(display, focus_return).is_ok_and(|s| s.eq("EcoPaste")) {
+                continue;
+            }
+
+            let mut app = FOREMOST_APPS.lock().unwrap();
+            let _ = app.insert(focus_return);
         }
     });
 }
 
-pub fn get_foreground_apps() -> Vec<u64> {
-    return FOREMOST_APPS.lock().unwrap().to_vec();
+pub fn get_foreground_apps() -> Option<u64> {
+    return FOREMOST_APPS.lock().unwrap().clone();
 }
