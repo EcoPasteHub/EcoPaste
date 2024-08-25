8fdf7872a32d1e4be272e5a45055efd39b70a7c3
kohai <i@kohai.top>
2024-08-25 16:17:06 +0800

    feat: 更新 `偏好设置-通用设置-权限设置` 国际化

--- src/pages/General/components/MacosPermissions/index.tsx
+++ src/pages/General/components/MacosPermissions/index.tsx
@@ -3,6 +3,8 @@ import ProList from "@/components/ProList";
 import ProListItem from "@/components/ProListItem";
 
 const MacosPermissions = () => {
+	const { t } = useTranslation();
+
 	const state = useReactive({
 		accessibilityPermissions: false,
 		fullDiskAccessPermissions: false,
@@ -37,11 +39,22 @@ const MacosPermissions = () => {
 
 		if (opened) return;
 
-		const yes = await ask("需要开启完全磁盘访问权限来实现文件预览", {
-			title: "需要开启完全磁盘访问权限",
-			okLabel: "去系统设置开启",
-			cancelLabel: "稍后开启",
-		});
+		const yes = await ask(
+			t(
+				"preference.settings.permission_settings.hints.confirm_full_disk_access",
+			),
+			{
+				title: t(
+					"preference.settings.permission_settings.label.confirm_full_disk_access",
+				),
+				okLabel: t(
+					"preference.settings.permission_settings.button.confirm_full_disk_access",
+				),
+				cancelLabel: t(
+					"preference.settings.permission_settings.button.cancel_full_disk_access",
+				),
+			},
+		);
 
 		if (!yes) return;
 
@@ -57,7 +70,7 @@ const MacosPermissions = () => {
 				{state[key] ? (
 					<div className="text-primary">
 						<Icon name="i-lucide:circle-check" />
-						已授权
+						{t("preference.settings.permission_settings.label.authorized")}
 					</div>
 				) : (
 					<div
@@ -65,7 +78,7 @@ const MacosPermissions = () => {
 						onMouseDown={mouseDownEvent}
 					>
 						<Icon name="i-lucide:circle-arrow-right" />
-						去授权
+						{t("preference.settings.permission_settings.button.authorize")}
 					</div>
 				)}
 			</div>
@@ -74,17 +87,25 @@ const MacosPermissions = () => {
 
 	return (
 		isMac() && (
-			<ProList header="权限设置">
+			<ProList header={t("preference.settings.permission_settings.title")}>
 				<ProListItem
-					title="辅助功能访问权限"
-					description="需要无障碍访问权限来操作剪切板内容"
+					title={t(
+						"preference.settings.permission_settings.label.accessibility_permissions",
+					)}
+					description={t(
+						"preference.settings.permission_settings.hints.accessibility_permissions",
+					)}
 				>
 					{renderStatus("accessibilityPermissions", checkAccessibility)}
 				</ProListItem>
 
 				<ProListItem
-					title="完全磁盘访问权限"
-					description="需要完全磁盘访问权限来实现文件预览"
+					title={t(
+						"preference.settings.permission_settings.label.full_disk_access_permissions",
+					)}
+					description={t(
+						"preference.settings.permission_settings.hints.full_disk_access_permissions",
+					)}
 				>
 					{renderStatus("fullDiskAccessPermissions", checkFullDiskAccess)}
 				</ProListItem>
