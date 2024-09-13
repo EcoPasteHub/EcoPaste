66e6fc28298c5ca53babb6dbb0f8618ed27bd86f
ayang <473033518@qq.com>
2024-09-13 11:53:06 +0800

    feat: 点击存储路径即可打开至文件资源管理器

--- src/pages/Backup/components/SavePath/index.tsx
+++ src/pages/Backup/components/SavePath/index.tsx
@@ -2,16 +2,14 @@ import ProList from "@/components/ProList";
 import ProListItem from "@/components/ProListItem";
 import { open } from "@tauri-apps/api/dialog";
 import { emit } from "@tauri-apps/api/event";
-import { join } from "@tauri-apps/api/path";
+import { join, sep } from "@tauri-apps/api/path";
 import { Button, Flex, message } from "antd";
 import { isString } from "antd/es/button";
 import type { FC } from "react";
-import { useSnapshot } from "valtio";
 import type { State } from "../..";
 
 const SavePath: FC<{ state: State }> = (props) => {
 	const { state } = props;
-	const { env } = useSnapshot(globalStore);
 
 	const handleClick = async () => {
 		try {
@@ -43,9 +41,16 @@ const SavePath: FC<{ state: State }> = (props) => {
 		<ProList header="存储路径">
 			<ProListItem
 				title={
-					<Flex vertical gap={2}>
+					<Flex vertical align="flex-start" gap={2}>
 						自定义存储路径
-						<span className="color-3 break-all text-12">{env.saveDataDir}</span>
+						<span
+							className="color-3 hover:color-primary cursor-pointer break-all text-12 transition"
+							onMouseDown={() => {
+								previewPath(getSaveDataDir());
+							}}
+						>
+							{getSaveDataDir().replace(new RegExp(`${sep}$`, "g"), "")}
+						</span>
 					</Flex>
 				}
 			>
