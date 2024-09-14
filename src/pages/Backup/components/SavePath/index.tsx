98243222843e7c73f77c44fc9ff4b78269bb3a61
ayang <473033518@qq.com>
2024-09-14 21:58:58 +0800

    refactor: 优化获取路径的相关代码

--- src/pages/Backup/components/SavePath/index.tsx
+++ src/pages/Backup/components/SavePath/index.tsx
@@ -3,23 +3,27 @@ import ProListItem from "@/components/ProListItem";
 import { NodeIndexOutlined, ReloadOutlined } from "@ant-design/icons";
 import { open } from "@tauri-apps/api/dialog";
 import { emit } from "@tauri-apps/api/event";
-import { dataDir, join } from "@tauri-apps/api/path";
+import { dataDir } from "@tauri-apps/api/path";
 import { Button, Flex, Space, Tooltip, message } from "antd";
 import { isString } from "antd/es/button";
+import { isEqual } from "lodash-es";
 import type { FC } from "react";
 import type { State } from "../..";
 
 const SavePath: FC<{ state: State }> = (props) => {
 	const { state } = props;
 	const { t } = useTranslation();
+	const [defaultDir, setDefaultDir] = useState("");
+
+	useMount(async () => {
+		setDefaultDir(await dataDir());
+	});
 
 	const handleChange = async (isDefault = false) => {
 		try {
-			const nextDir = isDefault
-				? await dataDir()
-				: await open({ directory: true });
+			const nextDir = isDefault ? defaultDir : await open({ directory: true });
 
-			if (!isString(nextDir) || getSaveDataDir().startsWith(nextDir)) return;
+			if (!isString(nextDir) || isEqualPath(nextDir)) return;
 
 			state.spinning = true;
 
@@ -27,7 +31,7 @@ const SavePath: FC<{ state: State }> = (props) => {
 
 			if (!dirName) return;
 
-			globalStore.env.saveDataDir = await join(nextDir, dirName);
+			globalStore.env.saveDataDir = joinPath(nextDir, dirName);
 
 			state.spinning = false;
 
@@ -43,6 +47,10 @@ const SavePath: FC<{ state: State }> = (props) => {
 		}
 	};
 
+	const isEqualPath = (nextDir = defaultDir) => {
+		return isEqual(joinPath(nextDir, getSaveDataDirName()), getSaveDataDir());
+	};
+
 	return (
 		<ProList header={t("preference.data_backup.storage_path.title")}>
 			<ProListItem
@@ -55,7 +63,7 @@ const SavePath: FC<{ state: State }> = (props) => {
 								previewPath(getSaveDataDir());
 							}}
 						>
-							{getSaveDataDir(false)}
+							{getSaveDataDir()}
 						</span>
 					</Flex>
 				}
@@ -74,6 +82,7 @@ const SavePath: FC<{ state: State }> = (props) => {
 						title={t("preference.data_backup.storage_path.hints.default_path")}
 					>
 						<Button
+							disabled={isEqualPath()}
 							icon={<ReloadOutlined className="text-14!" />}
 							onClick={() => handleChange(true)}
 						/>
---
+++ src/utils/path.ts
@@ -0,0 +1,59 @@
+import { sep } from "@tauri-apps/api/path";
+import { last } from "lodash-es";
+
+/**
+ * 连接相应系统的路径
+ * @param paths 路径数组
+ */
+export const joinPath = (...paths: string[]) => {
+	const joinPaths = paths.map((path) => {
+		if (path.endsWith(sep)) {
+			return path.slice(0, -1);
+		}
+
+		return path;
+	});
+
+	return joinPaths.join(sep);
+};
+
+/**
+ * 获取存储数据的目录
+ * @param endWithSep 结尾是否包含分隔符
+ */
+export const getSaveDataDir = () => {
+	let { saveDataDir = "" } = globalStore.env;
+
+	saveDataDir = joinPath(saveDataDir, "");
+
+	return saveDataDir.slice(0, -1);
+};
+
+/**
+ * 获取存储图片的目录
+ */
+export const getSaveImageDir = () => {
+	return joinPath(getSaveDataDir(), "images", "");
+};
+
+/**
+ * 获取图片内容的存储路径
+ * @param file 文件名
+ */
+export const getSaveImagePath = (fileName: string) => {
+	return joinPath(getSaveImageDir(), fileName);
+};
+
+/**
+ * 备份数据时全局数据的存储路径
+ */
+export const getBackupStorePath = () => {
+	return joinPath(getSaveDataDir(), "backup-store");
+};
+
+/**
+ * 存储数据的目录名
+ */
+export const getSaveDataDirName = () => {
+	return last(getSaveDataDir().split(sep)) as string;
+};
