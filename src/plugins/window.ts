a8f533c9cf200f6984d9d585381847d94ae6e735
AuroraTea <1352685369@qq.com>
2024-09-28 21:58:17 +0800

    fix: 改善快捷键无法呼出窗体问题

--- src/plugins/window.ts
+++ src/plugins/window.ts
@@ -1,5 +1,6 @@
 import { WINDOW_PLUGIN } from "@/constants";
 import type { WindowLabel } from "@/types/plugin";
+import { isLinux } from "@/utils/is";
 import { invoke } from "@tauri-apps/api";
 import { emit } from "@tauri-apps/api/event";
 import {
@@ -7,6 +8,7 @@ import {
 	appWindow,
 	availableMonitors,
 } from "@tauri-apps/api/window";
+
 /**
  * 显示窗口
  */
@@ -28,67 +30,130 @@ export const hideWindow = () => {
 /**
  * 切换窗口的显示和隐藏
  */
-export const toggleWindowVisible = async () => {
-	const focused = await appWindow.isFocused();
+export const toggleWindowVisible = {
+	common: async () => {
+		const focused = await appWindow.isFocused();
 
-	if (appWindow.label === WINDOW_LABEL.MAIN) {
-		const { window } = clipboardStore;
+		if (appWindow.label === WINDOW_LABEL.MAIN) {
+			const { window } = clipboardStore;
 
-		if (window.style === "float") {
-			if (!focused && window.position !== "remember") {
-				const monitors = await availableMonitors();
+			if (window.style === "float") {
+				if (!focused && window.position !== "remember") {
+					const monitors = await availableMonitors();
 
-				if (!monitors.length) return;
+					if (!monitors.length) return;
 
-				const { width, height } = await appWindow.innerSize();
+					const { width, height } = await appWindow.innerSize();
 
-				const [x, y] = await getMouseCoords();
+					const [x, y] = await getMouseCoords();
 
-				for (const monitor of monitors) {
-					const {
-						scaleFactor,
-						position: { x: posX, y: posY },
-						size: { width: screenWidth, height: screenHeight },
-					} = monitor;
+					for (const monitor of monitors) {
+						const {
+							scaleFactor,
+							position: { x: posX, y: posY },
+							size: { width: screenWidth, height: screenHeight },
+						} = monitor;
 
-					const factor = isMac() ? scaleFactor : 1;
+						const factor = isMac() ? scaleFactor : 1;
 
-					let coordX = x * factor;
-					let coordY = y * factor;
+						let coordX = x * factor;
+						let coordY = y * factor;
 
-					if (
-						coordX < posX ||
-						coordY < posY ||
-						coordX > posX + screenWidth ||
-						coordY > posY + screenHeight
-					) {
-						continue;
+						if (
+							coordX < posX ||
+							coordY < posY ||
+							coordX > posX + screenWidth ||
+							coordY > posY + screenHeight
+						) {
+							continue;
+						}
+
+						if (window.position === "follow") {
+							coordX = Math.min(coordX, posX + screenWidth - width);
+							coordY = Math.min(coordY, posY + screenHeight - height);
+						} else if (window.position === "center") {
+							coordX = posX + (screenWidth - width) / 2;
+							coordY = posY + (screenHeight - height) / 2;
+						}
+
+						appWindow.setPosition(new PhysicalPosition(coordX, coordY));
+
+						break;
 					}
-
-					if (window.position === "follow") {
-						coordX = Math.min(coordX, posX + screenWidth - width);
-						coordY = Math.min(coordY, posY + screenHeight - height);
-					} else if (window.position === "center") {
-						coordX = posX + (screenWidth - width) / 2;
-						coordY = posY + (screenHeight - height) / 2;
-					}
-
-					appWindow.setPosition(new PhysicalPosition(coordX, coordY));
-
-					break;
 				}
+			} else {
+				// TODO: dock 风格的位置
 			}
-		} else {
-			// TODO: dock 风格的位置
 		}
-	}
 
-	if (focused) {
-		hideWindow();
-	} else {
-		showWindow();
-	}
-};
+		if (focused) {
+			hideWindow();
+		} else {
+			showWindow();
+		}
+	},
+	linux: async () => {
+		const visible = await appWindow.isVisible();
+
+		if (appWindow.label === WINDOW_LABEL.MAIN) {
+			const { window } = clipboardStore;
+
+			if (window.style === "float") {
+				if (!visible && window.position !== "remember") {
+					const monitors = await availableMonitors();
+
+					if (!monitors.length) return;
+
+					const { width, height } = await appWindow.innerSize();
+
+					const [x, y] = await getMouseCoords();
+
+					for (const monitor of monitors) {
+						const {
+							scaleFactor,
+							position: { x: posX, y: posY },
+							size: { width: screenWidth, height: screenHeight },
+						} = monitor;
+
+						const factor = isMac() ? scaleFactor : 1;
+
+						let coordX = x * factor;
+						let coordY = y * factor;
+
+						if (
+							coordX < posX ||
+							coordY < posY ||
+							coordX > posX + screenWidth ||
+							coordY > posY + screenHeight
+						) {
+							continue;
+						}
+
+						if (window.position === "follow") {
+							coordX = Math.min(coordX, posX + screenWidth - width);
+							coordY = Math.min(coordY, posY + screenHeight - height);
+						} else if (window.position === "center") {
+							coordX = posX + (screenWidth - width) / 2;
+							coordY = posY + (screenHeight - height) / 2;
+						}
+
+						appWindow.setPosition(new PhysicalPosition(coordX, coordY));
+
+						break;
+					}
+				}
+			} else {
+				// TODO: dock 风格的位置
+			}
+		}
+
+		if (visible) {
+			hideWindow();
+		} else {
+			showWindow();
+		}
+	},
+}[isLinux() ? "linux" : "common"];
 
 /**
  * 显示任务栏图标
