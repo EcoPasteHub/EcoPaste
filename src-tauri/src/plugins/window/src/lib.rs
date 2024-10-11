e7bbc57e1aea8e6dfc165a46eeceea30c6291800
ayang <473033518@qq.com>
2024-10-11 11:43:10 +0800

    style: 删除无用代码

--- src-tauri/src/plugins/window/src/lib.rs
+++ src-tauri/src/plugins/window/src/lib.rs
@@ -10,11 +10,6 @@ pub use commands::*;
 
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("eco-window")
-        .setup(|_app, _api| {
-            println!("window plugin loaded");
-
-            Ok(())
-        })
         .invoke_handler(generate_handler![
             commands::show_window,
             commands::hide_window,
