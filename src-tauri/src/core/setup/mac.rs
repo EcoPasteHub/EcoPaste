90fd10449a3dd61aff300b0da9523c2ad72cb91d
ayang <473033518@qq.com>
2024-10-14 11:24:41 +0800

    refactor: 以配置项的方式实现 macos 偏好设置窗口的磨砂效果

--- src-tauri/src/core/setup/mac.rs
+++ src-tauri/src/core/setup/mac.rs
@@ -1,17 +1,8 @@
 use cocoa::appkit::{NSMainMenuWindowLevel, NSWindow};
 use cocoa::base::id;
-use tauri::window::{Effect, EffectState, EffectsBuilder};
 use tauri::{ActivationPolicy, App, WebviewWindow};
 
-pub fn platform(app: &mut App, main_window: WebviewWindow, preference_window: WebviewWindow) {
-    let _ = preference_window.set_effects(
-        EffectsBuilder::new()
-            .effect(Effect::Sidebar)
-            .state(EffectState::Active)
-            .radius(10.0)
-            .build(),
-    );
-
+pub fn platform(app: &mut App, main_window: WebviewWindow, _preference_window: WebviewWindow) {
     // 隐藏 mac 的程序坞图标：https://github.com/tauri-apps/tauri/issues/4852#issuecomment-1312716378
     app.set_activation_policy(ActivationPolicy::Accessory);
 
