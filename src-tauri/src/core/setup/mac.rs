8f2d9def4a5043026342f926d950900b5d2b5546
ayang <473033518@qq.com>
2024-10-23 09:20:32 +0800

    feat: to macos ns_panel

--- src-tauri/src/core/setup/mac.rs
+++ src-tauri/src/core/setup/mac.rs
@@ -1,15 +1,27 @@
-use cocoa::appkit::{NSMainMenuWindowLevel, NSWindow};
-use cocoa::base::id;
 use tauri::{ActivationPolicy, App, WebviewWindow};
+use tauri_nspanel::{
+    cocoa::appkit::{NSMainMenuWindowLevel, NSWindowCollectionBehavior},
+    WebviewWindowExt,
+};
+
+#[allow(non_upper_case_globals)]
+const NSWindowStyleMaskNonActivatingPanel: i32 = 1 << 7;
+#[allow(non_upper_case_globals)]
+const NSResizableWindowMask: i32 = 1 << 3;
 
 pub fn platform(app: &mut App, main_window: WebviewWindow, _preference_window: WebviewWindow) {
     // 隐藏 mac 的程序坞图标：https://github.com/tauri-apps/tauri/issues/4852#issuecomment-1312716378
     app.set_activation_policy(ActivationPolicy::Accessory);
 
-    unsafe {
-        let ns_window = main_window.ns_window().unwrap() as id;
+    let panel = main_window.to_panel().unwrap();
 
-        // 让窗口在程序坞和菜单栏之上
-        ns_window.setLevel_(NSMainMenuWindowLevel as i64 + 1);
-    }
+    panel.set_level(NSMainMenuWindowLevel + 1);
+
+    panel.set_style_mask(NSWindowStyleMaskNonActivatingPanel | NSResizableWindowMask);
+
+    panel.set_collection_behaviour(
+        NSWindowCollectionBehavior::NSWindowCollectionBehaviorCanJoinAllSpaces
+            | NSWindowCollectionBehavior::NSWindowCollectionBehaviorStationary
+            | NSWindowCollectionBehavior::NSWindowCollectionBehaviorFullScreenAuxiliary,
+    );
 }
--- src-tauri/src/plugins/window/src/commands.rs
+++ src-tauri/src/plugins/window/src/commands.rs
@@ -1,4 +1,5 @@
 use tauri::{async_runtime::spawn, command, AppHandle, Manager, Runtime, WebviewWindow};
+use tauri_nspanel::ManagerExt;
 
 // 主窗口的label
 pub static MAIN_WINDOW_LABEL: &str = "main";
@@ -9,16 +10,36 @@ pub static MAIN_WINDOW_TITLE: &str = "EcoPaste";
 
 // 显示窗口
 #[command]
-pub async fn show_window<R: Runtime>(window: WebviewWindow<R>) {
-    window.show().unwrap();
-    window.unminimize().unwrap();
-    window.set_focus().unwrap();
+pub async fn show_window<R: Runtime>(app_handle: AppHandle<R>, window: WebviewWindow<R>) {
+    if cfg!(target_os = "macos") && window.label() == MAIN_WINDOW_LABEL {
+        let handle = app_handle.clone();
+
+        let _ = app_handle.run_on_main_thread(move || {
+            let panel = handle.get_webview_panel(MAIN_WINDOW_LABEL).unwrap();
+
+            panel.show();
+        });
+    } else {
+        window.show().unwrap();
+        window.unminimize().unwrap();
+        window.set_focus().unwrap();
+    }
 }
 
 // 隐藏窗口
 #[command]
-pub async fn hide_window<R: Runtime>(window: WebviewWindow<R>) {
-    window.hide().unwrap();
+pub async fn hide_window<R: Runtime>(app_handle: AppHandle<R>, window: WebviewWindow<R>) {
+    if cfg!(target_os = "macos") && window.label() == MAIN_WINDOW_LABEL {
+        let handle = app_handle.clone();
+
+        let _ = app_handle.run_on_main_thread(move || {
+            let panel = handle.get_webview_panel(MAIN_WINDOW_LABEL).unwrap();
+
+            panel.order_out(None);
+        });
+    } else {
+        window.hide().unwrap();
+    }
 }
 
 // 显示任务栏图标
@@ -46,9 +67,9 @@ pub fn show_taskbar_icon<R: Runtime>(
 pub fn show_main_window(app_handle: &AppHandle) {
     let window = app_handle.get_webview_window(MAIN_WINDOW_LABEL).unwrap();
 
-    spawn(async move {
-        show_window(window).await;
-    });
+    // spawn(async move {
+    //     show_window(window).await;
+    // });
 }
 
 // 显示偏好设置窗口
@@ -57,7 +78,7 @@ pub fn show_preference_window(app_handle: &AppHandle) {
         .get_webview_window(PREFERENCE_WINDOW_LABEL)
         .unwrap();
 
-    spawn(async move {
-        show_window(window).await;
-    });
+    // spawn(async move {
+    //     show_window(window).await;
+    // });
 }
