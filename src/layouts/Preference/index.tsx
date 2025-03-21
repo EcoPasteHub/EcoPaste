58d4df9425cf87652ff2ad1cbfc0220ea61966db
alexju <alexju@tencent.com>
2025-03-21 17:46:27 +0800

    refactor: 使用 Layout 组件重构设置页面

--- src/layouts/Preference/index.tsx
+++ src/layouts/Preference/index.tsx
@@ -1,4 +1,5 @@
 import Icon from "@/components/Icon";
+import Scrollbar from "@/components/Scrollbar";
 import Tray from "@/components/Tray";
 import UpdateApp from "@/components/UpdateApp";
 import About from "@/pages/About";
@@ -8,10 +9,18 @@ import General from "@/pages/General";
 import History from "@/pages/History";
 import Shortcut from "@/pages/Shortcut";
 import { emit } from "@tauri-apps/api/event";
-import { Flex, Tabs, type TabsProps } from "antd";
-import clsx from "clsx";
+import { Layout, Menu } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { Content } from "antd/lib/layout/layout";
 import { useSnapshot } from "valtio";
 import styles from "./index.module.scss";
+
+interface TabItem {
+	key: string;
+	label: string;
+	icon: string;
+	children: React.ReactNode;
+}
 
 const PreferenceLayout = () => {
 	const { t } = useTranslation();
@@ -35,7 +44,7 @@ const PreferenceLayout = () => {
 	// 监听快捷键切换窗口显隐
 	useRegister(toggleWindowVisible, [shortcut.preference]);
 
-	const tabItems: TabsProps["items"] = [
+	const tabItems: TabItem[] = [
 		{
 			key: "clipboard",
 			label: t("preference.menu.title.clipboard"),
@@ -46,14 +55,12 @@ const PreferenceLayout = () => {
 			key: "history",
 			label: t("preference.menu.title.history"),
 			icon: "i-lucide:history",
-			forceRender: true,
 			children: <History />,
 		},
 		{
 			key: "general",
 			label: t("preference.menu.title.general"),
 			icon: "i-lucide:bolt",
-			forceRender: true,
 			children: <General />,
 		},
 		{
@@ -76,43 +83,6 @@ const PreferenceLayout = () => {
 		},
 	];
 
-	const renderTabBar: TabsProps["renderTabBar"] = () => {
-		return (
-			<Flex
-				data-tauri-drag-region
-				vertical
-				gap="small"
-				className={clsx("h-full w-200 p-12", [
-					isMac() ? "pt-32" : "bg-color-1",
-				])}
-				onClick={(event) => event.stopPropagation()}
-			>
-				{tabItems.map((item) => {
-					const { key, label, icon } = item;
-
-					return (
-						<Flex
-							key={key}
-							align="center"
-							gap="small"
-							className={clsx(
-								"cursor-pointer rounded-8 p-12 p-r-0 text-color-2 transition hover:bg-color-4",
-								{
-									"bg-primary! text-white!": activeKey === key,
-								},
-							)}
-							onClick={() => handleTabItemClick(key)}
-						>
-							<Icon name={icon as string} size={20} />
-
-							<span className="font-bold">{label}</span>
-						</Flex>
-					);
-				})}
-			</Flex>
-		);
-	};
-
 	// 配置项变化通知其它窗口和本地存储
 	const handleStoreChanged = async () => {
 		emit(LISTEN_KEY.STORE_CHANGED, { globalStore, clipboardStore });
@@ -120,27 +90,39 @@ const PreferenceLayout = () => {
 		saveStore();
 	};
 
-	// 切换选项卡
-	const handleTabItemClick = (key: string) => {
+	const menuItems = tabItems.map((item) => ({
+		key: item.key,
+		label: <span className="font-bold">{item.label}</span>,
+		icon: <Icon name={item.icon as string} size={20} />,
+	}));
+
+	// 根据 activeKey 获取当前要显示的内容
+	const getCurrentContent = () => {
+		const currentTab = tabItems.find((item) => item.key === activeKey);
+		return currentTab?.children;
+	};
+
+	// 处理菜单点击事件
+	const handleMenuClick = ({ key }: { key: string }) => {
 		setActiveKey(key);
-
-		raf(() => {
-			const element = document.querySelector(".ant-tabs-content-holder");
-
-			element?.scrollTo({ top: 0, behavior: "smooth" });
-		});
 	};
 
 	return (
 		<>
-			<Tabs
-				animated
-				activeKey={activeKey}
-				tabPosition="left"
-				items={tabItems}
-				className={styles.root}
-				renderTabBar={renderTabBar}
-			/>
+			<Layout className={styles.root}>
+				<Sider>
+					<Menu
+						className="h-full p-t-24"
+						mode="inline"
+						items={menuItems}
+						selectedKeys={[activeKey]}
+						onClick={handleMenuClick}
+					/>
+				</Sider>
+				<Content className="p-r-2 py-4">
+					<Scrollbar className="h-full p-10">{getCurrentContent()}</Scrollbar>
+				</Content>
+			</Layout>
 
 			<Tray />
 
