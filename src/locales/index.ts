f56cd6e20170becd4d3d8bb5d700f1aeaf56bece
KietNT <113796420+TanNhatCMS@users.noreply.github.com>
2025-07-30 22:40:00 +0700

    feat: Add Vietnamese language support
    Introduces Vietnamese (vi-VN) localization by updating constants, locale resources, and language selection options. Includes new vi-VN translation file and integrates Ant Design's Vietnamese locale.

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,12 +2,14 @@ import type { Language } from "@/types/store";
 import type { Locale as AntdLocale } from "antd/es/locale";
 import antdEnUS from "antd/locale/en_US";
 import antdJaJP from "antd/locale/ja_JP";
+import antdViVN from "antd/locale/vi_VN";
 import antdZhCN from "antd/locale/zh_CN";
 import antdZhTW from "antd/locale/zh_TW";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enUS from "./en-US.json";
 import jaJP from "./ja-JP.json";
+import viVN from "./vi-VN.json";
 import zhCN from "./zh-CN.json";
 import zhTW from "./zh-TW.json";
 
@@ -24,6 +26,9 @@ i18n.use(initReactI18next).init({
 		},
 		[LANGUAGE.JA_JP]: {
 			translation: jaJP,
+		},
+		[LANGUAGE.VI_VN]: {
+			translation: viVN,
 		},
 	},
 	lng: LANGUAGE.ZH_CN,
@@ -42,6 +47,7 @@ export const getAntdLocale = (language: Language = LANGUAGE.ZH_CN) => {
 		[LANGUAGE.ZH_TW]: antdZhTW,
 		[LANGUAGE.EN_US]: antdEnUS,
 		[LANGUAGE.JA_JP]: antdJaJP,
+		[LANGUAGE.VI_VN]: antdViVN,
 	};
 
 	return antdLanguage[language];
