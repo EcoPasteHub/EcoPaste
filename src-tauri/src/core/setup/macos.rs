0109fb681bc19a36361a87419d8580388c4e083c
ayangweb <75017711+ayangweb@users.noreply.github.com>
2025-10-20 14:42:49 +0800

    chore: 升级 tauri-nspanel 并重构 macOS 窗口管理 (#1170)

--- src-tauri/src/core/setup/macos.rs
+++ src-tauri/src/core/setup/macos.rs
@@ -1,85 +1,89 @@
-#![allow(deprecated)]
-use tauri::{AppHandle, Emitter, EventTarget, WebviewWindow};
-use tauri_nspanel::{cocoa::appkit::NSWindowCollectionBehavior, panel_delegate, WebviewWindowExt};
+use tauri::{AppHandle, Emitter, EventTarget, Manager, WebviewWindow};
+use tauri_nspanel::{tauri_panel, CollectionBehavior, PanelLevel, StyleMask, WebviewWindowExt};
 use tauri_plugin_eco_window::MAIN_WINDOW_LABEL;
 
-#[allow(non_upper_case_globals)]
-const NSWindowStyleMaskNonActivatingPanel: i32 = 1 << 7;
-#[allow(non_upper_case_globals)]
-const NSResizableWindowMask: i32 = 1 << 3;
 const WINDOW_FOCUS_EVENT: &str = "tauri://focus";
 const WINDOW_BLUR_EVENT: &str = "tauri://blur";
 const WINDOW_MOVED_EVENT: &str = "tauri://move";
 const WINDOW_RESIZED_EVENT: &str = "tauri://resize";
+
+tauri_panel! {
+    panel!(NsPanel {
+        config: {
+            is_floating_panel: true,
+            can_become_key_window: true,
+            can_become_main_window: false
+        }
+    })
+
+    panel_event!(NsPanelEventHandler {
+        window_did_become_key(notification: &NSNotification) -> (),
+        window_did_resign_key(notification: &NSNotification) -> (),
+        window_did_resize(notification: &NSNotification) -> (),
+        window_did_move(notification: &NSNotification) -> (),
+    })
+}
 
 pub fn platform(
     app_handle: &AppHandle,
     main_window: WebviewWindow,
     _preference_window: WebviewWindow,
 ) {
-    // macos window 转 ns_panel 插件
     let _ = app_handle.plugin(tauri_nspanel::init());
 
-    // 隐藏 mac 的程序坞图标：https://github.com/tauri-apps/tauri/pull/13185
     let _ = app_handle.set_dock_visibility(false);
 
-    // 把 ns_window 转换为 ns_panel
-    let panel = main_window.to_panel().unwrap();
+    let panel = main_window.to_panel::<NsPanel>().unwrap();
 
-    // 让窗口在程序坞之上
-    panel.set_level(20);
+    panel.set_level(PanelLevel::Dock.value());
 
-    // 不抢占其它窗口的焦点和支持缩放
-    panel.set_style_mask(NSWindowStyleMaskNonActivatingPanel | NSResizableWindowMask);
+    panel.set_style_mask(StyleMask::empty().resizable().nonactivating_panel().into());
 
-    // 在各个桌面空间、全屏中共享窗口
-    panel.set_collection_behaviour(
-        NSWindowCollectionBehavior::NSWindowCollectionBehaviorCanJoinAllSpaces
-            | NSWindowCollectionBehavior::NSWindowCollectionBehaviorStationary
-            | NSWindowCollectionBehavior::NSWindowCollectionBehaviorFullScreenAuxiliary,
+    panel.set_collection_behavior(
+        CollectionBehavior::new()
+            .stationary()
+            .can_join_all_spaces()
+            .full_screen_auxiliary()
+            .into(),
     );
 
-    // 定义面板的委托 (delegate)，用于监听面板窗口的事件
-    let delegate = panel_delegate!(EcoPanelDelegate {
-        window_did_become_key,
-        window_did_resign_key,
-        window_did_resize,
-        window_did_move
-    });
+    let handler = NsPanelEventHandler::new();
 
-    // 为 delegate 设置事件监听器
-    delegate.set_listener(Box::new(move |delegate_name: String| {
+    let window = main_window.clone();
+    handler.window_did_become_key(move |_| {
         let target = EventTarget::labeled(MAIN_WINDOW_LABEL);
 
-        let window_move_event = || {
-            if let Ok(position) = main_window.outer_position() {
-                let _ = main_window.emit_to(target.clone(), WINDOW_MOVED_EVENT, position);
-            }
-        };
+        let _ = window.emit_to(target, WINDOW_FOCUS_EVENT, true);
+    });
 
-        match delegate_name.as_str() {
-            // 当窗口获得键盘焦点时调用
-            "window_did_become_key" => {
-                let _ = main_window.emit_to(target, WINDOW_FOCUS_EVENT, true);
-            }
-            // 当窗口失去键盘焦点时调用
-            "window_did_resign_key" => {
-                let _ = main_window.emit_to(target, WINDOW_BLUR_EVENT, true);
-            }
-            // 当窗口大小改变时调用
-            "window_did_resize" => {
-                window_move_event();
+    let window = main_window.clone();
+    handler.window_did_resign_key(move |_| {
+        let target = EventTarget::labeled(MAIN_WINDOW_LABEL);
 
-                if let Ok(size) = main_window.inner_size() {
-                    let _ = main_window.emit_to(target, WINDOW_RESIZED_EVENT, size);
-                }
-            }
-            // 当窗口位置改变时调用
-            "window_did_move" => window_move_event(),
-            _ => (),
+        let _ = window.emit_to(target, WINDOW_BLUR_EVENT, true);
+    });
+
+    let window = main_window.clone();
+    handler.window_did_resize(move |_| {
+        let target = EventTarget::labeled(MAIN_WINDOW_LABEL);
+
+        if let Ok(position) = window.outer_position() {
+            let _ = window.emit_to(target.clone(), WINDOW_MOVED_EVENT, position);
         }
-    }));
 
-    // 设置窗口的委托对象，用于处理窗口的事件。
-    panel.set_delegate(delegate);
+        if let Ok(size) = window.inner_size() {
+            let _ = window.emit_to(target, WINDOW_RESIZED_EVENT, size);
+        }
+    });
+
+    let window = main_window.clone();
+    handler.window_did_move(move |_| {
+        let target = EventTarget::labeled(MAIN_WINDOW_LABEL);
+
+        if let Ok(position) = window.outer_position() {
+            let _ = window.emit_to(target.clone(), WINDOW_MOVED_EVENT, position);
+        }
+    });
+
+    panel.set_event_handler(Some(handler.as_ref()));
 }
