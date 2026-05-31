67e0f907c934f4e4bdca500774c061995506f5fe
ayangweb <473033518@qq.com>
2026-05-31 17:05:23 +0800

    feat: update tauri configuration for preference window and enhance main app rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
 import ReactDOM from "react-dom/client";
+import App from "./App";
+
 import "virtual:uno.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />,
+);
