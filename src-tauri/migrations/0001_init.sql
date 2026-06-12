bc3ffbdee62df7e5917e7c9160d5da3d10f5d960
ayangweb <473033518@qq.com>
2026-06-12 10:55:54 +0800

    refactor(db): merge FTS migration into 0001_init

--- src-tauri/migrations/0001_init.sql
+++ src-tauri/migrations/0001_init.sql
@@ -53,3 +53,28 @@ CREATE TABLE file_type_icons (
     updated_at  TEXT NOT NULL,
     PRIMARY KEY (cache_key, platform)
 );
+
+CREATE VIRTUAL TABLE clipboard_items_fts USING fts5(
+    search_text,
+    note,
+    content='clipboard_items',
+    content_rowid='rowid',
+    tokenize='trigram'
+);
+
+CREATE TRIGGER clipboard_items_ai AFTER INSERT ON clipboard_items BEGIN
+    INSERT INTO clipboard_items_fts(rowid, search_text, note)
+    VALUES (new.rowid, new.search_text, new.note);
+END;
+
+CREATE TRIGGER clipboard_items_ad AFTER DELETE ON clipboard_items BEGIN
+    INSERT INTO clipboard_items_fts(clipboard_items_fts, rowid, search_text, note)
+    VALUES ('delete', old.rowid, old.search_text, old.note);
+END;
+
+CREATE TRIGGER clipboard_items_au AFTER UPDATE ON clipboard_items BEGIN
+    INSERT INTO clipboard_items_fts(clipboard_items_fts, rowid, search_text, note)
+    VALUES ('delete', old.rowid, old.search_text, old.note);
+    INSERT INTO clipboard_items_fts(rowid, search_text, note)
+    VALUES (new.rowid, new.search_text, new.note);
+END;
