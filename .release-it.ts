52cce990b815a289f958c3747aca507e161b1b2b
ayangweb <473033518@qq.com>
2026-07-01 12:09:01 +0800

    feat: add release version management

---
+++ .release-it.ts
@@ -0,0 +1,42 @@
+const changelogFile = "CHANGELOG.md";
+const versionVariable = "$" + "{version}";
+
+import type { Config } from "release-it";
+
+const config = {
+  git: {
+    commitMessage: `chore: release v${versionVariable}`,
+    getLatestTagFromAllRefs: true,
+    tagAnnotation: `Release v${versionVariable}`,
+    tagName: `v${versionVariable}`,
+  },
+  github: {
+    release: false,
+  },
+  npm: {
+    publish: false,
+  },
+  plugins: {
+    "@release-it/bumper": {
+      out: [
+        {
+          file: "src-tauri/Cargo.toml",
+          path: "package.version",
+        },
+        {
+          file: "src-tauri/Cargo.lock",
+          path: "package.0.version",
+        },
+      ],
+    },
+    "@release-it/conventional-changelog": {
+      ignoreRecommendedBump: true,
+      infile: changelogFile,
+      preset: {
+        name: "conventionalcommits",
+      },
+    },
+  },
+} satisfies Config;
+
+export default config;
